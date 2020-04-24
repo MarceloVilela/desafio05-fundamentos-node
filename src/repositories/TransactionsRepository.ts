f7a7f5be8c42099c1e9d9e453e030b23d91cc4d7
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2020-04-24 20:30:10 -0300

    Resolucao do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,13 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +22,47 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    function reducerTransaction(accumulator: number, value: number): number {
+      return accumulator + value;
+    }
+
+    const transactionsIncome = this.transactions.filter(
+      item => item.type === 'income',
+    );
+
+    const transactionsOutcome = this.transactions.filter(
+      item => item.type === 'outcome',
+    );
+
+    const income =
+      transactionsIncome.length > 0
+        ? transactionsIncome.map(item => item.value).reduce(reducerTransaction)
+        : 0;
+    const outcome =
+      transactionsOutcome.length > 0
+        ? transactionsOutcome.map(item => item.value).reduce(reducerTransaction)
+        : 0;
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
